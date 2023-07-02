197e462c96720c17f81f2119245fb3fa79e2d44c
ivanovem <ivanov_em@inbox.ru>
2023-07-02 09:57:13 +0500

    added PostList

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './styles/App.css';
+import PostItem from './components/PostItem';
+import { useState } from 'react';
+import PostList from './components/PostList';
+
+function App() {
+  const [posts, setPosts] = useState([
+    {id:1, title: 'JavaScript', body: 'Description'},
+    {id:2, title: 'JavaScript 2', body: 'Description'},
+    {id:3, title: 'JavaScript 3', body: 'Description'}
+  ])
+  return (
+    <div className="App">
+      
+      <PostList posts={posts} title="Список постов 1"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PostList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PostItem from "./PostItem";
+
+
+function PostList({posts, title}) {
+   
+    return (
+    <div className='post'>
+        <h1 style={{textAlign: "center"}}>
+            {title}
+        </h1>
+        {posts.map(post =>
+        <PostItem post={post} key={post.id}/>
+        )}
+    </div>
+    );
+  }
+  
+  export default PostList;
