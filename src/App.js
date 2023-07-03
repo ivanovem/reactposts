a678df7389798c734605fdde2608322beacd59f2
ivanovem <ivanov_em@inbox.ru>
2023-07-03 20:21:18 +0500

    added ModalWindow

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import PostList from './components/PostList';
 
 import PostForm from './components/UI/PostForm';
 import PostFilter from './components/PostFilter';
+import MyModal from './components/UI/MyModal/MyModal';
+import MyButton from './components/UI/button/MyButton';
 
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
   ])
 
   const [filter, setFilter] = useState({sort:'', query:''})
+  const [modal, setModal] = useState(false);
 
   // const [selectedSort, setSelectedSort] = useState('');
   // const [searchQuery, setSearchQuery] = useState('');
@@ -34,28 +37,26 @@ function App() {
 
   const createPost= (newPost)=>{
     setPosts([...posts, newPost]);
+    setModal(false);
   }
   
   const removePost = (post)=>{
     setPosts(posts.filter(p=> p.id!==post.id))
   }
 
-  // const sortPosts = (sort)=>{
-  //   setSelectedSort(sort);
-  // }
-
   return (
     <div className="App">
-      <PostForm create={createPost}/>
+      <MyButton style={{marginTop: "30px"}} onClick={()=>setModal(true)}>
+        Создать пользователя
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost}/>
+      </MyModal>
       <hr style={{margin: "15px 0px"}}/>
       <PostFilter filter={filter} setFilter={setFilter}/>
-      {sortedAndSearchedPosts.length!==0
-        ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов 1"/>
-        : <h1 style={{textAlign:"center"}}>
-            Посты не найдены
-          </h1>
-        
-      } 
+
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов 1"/>
+     
     </div>
   );
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -3,7 +3,13 @@ import PostItem from "./PostItem";
 
 
 function PostList({posts, title, remove}) {
-   
+    if (!posts.length) {
+        return (
+        <h1 style={{textAlign:"center"}}>
+            Посты не найдены
+        </h1>
+        )
+    }
     return (
     <div>
         <h1 style={{textAlign: "center"}}>
